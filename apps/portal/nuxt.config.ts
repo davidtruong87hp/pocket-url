aefb6db3860ea2bb5c8dd31404e0594c5ba0cb6d
davidtruong87hp <truongquangchien87@icloud.com>
2026-01-18 21:41:53 +0700

    added some Nuxt core modules

--- apps/portal/nuxt.config.ts
+++ apps/portal/nuxt.config.ts
@@ -1,8 +1,32 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
   devServer: {
     port: 8888,
   },
+
+  css: ['~/assets/css/main.css'],
+
+  // App config
+  app: {
+    head: {
+      title: 'Pocket URL - URL Shortener for your needs',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Modern URL Shortener Platform' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  modules: ['@nuxt/image', '@nuxt/fonts'],
 })
